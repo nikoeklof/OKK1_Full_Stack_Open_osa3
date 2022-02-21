9f1f88d6ad62e33b9108a274f9baa4d5a6b7c2f8
Niko / Nigey <dataaja.96@hotmail.com>
2022-02-21 12:31:28 +0200

    osa3 done

---
+++ puhelinluettelo ja tietokanta full stack step8/mongo.js
@@ -0,0 +1,40 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+const mongoose = require('mongoose')
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://nigey:${password}@cluster0.3oayx.mongodb.net/persons?retryWrites=true&w=majority`
+
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+})
+
+const Person = mongoose.model('people', personSchema)
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4],
+  id: Math.floor(Math.random() * 9999)
+})
+if (process.argv.length === 5) {
+  // eslint-disable-next-line no-unused-vars
+  person.save().then(result => {
+    console.log(`person ${process.argv[3]} added to phonebook`)
+    mongoose.connection.close()
+  })
+} else if (process.argv.length === 3) {
+  Person.find({}).then(persons => {
+    persons.forEach(person => {
+      console.log(`name: ${person.name} number: ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+} else {
+  console.log('Check parameters!!')
+  process.exit(1)
+}
+
